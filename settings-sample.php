12c58649ecd8c96484d75dc3fd8e00fc6d555ab7
Alexander Nielsen <hitecherik@hitecherik.site11.com>
2014-02-02 19:01:11 +0000

    Added sample settings file.

---
+++ settings-sample.php
@@ -0,0 +1,19 @@
+<?php
+	// MySQL Settings
+	$mysql = array(
+		"username" => "hitecherik", // mysql username
+		"password" => "programming", // mysql password
+		"database-name" => "blankblog", // database name
+		"table-name" => "posts", // table name (doesn't have to exist)
+		"host" => "localhost", // mysql host - ALWAYS in quotes
+	);
+	
+	// password
+	$password = "myblogpass";
+
+	// blog title
+	$blogtitle = "The Blog of the Century";
+
+	// root url - where the index.php file is located
+	$blogurl = "http://some-random-site.com/";
+?>
